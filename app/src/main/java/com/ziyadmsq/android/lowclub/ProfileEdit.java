1e30bd7ab42f3bf7b4d34aa7758478f19c48ed93
ziyadalqahtani <ziyadmsq@gmail.com>
2019-02-19 23:46:22 +0300

    trying out

---
+++ app/src/main/java/com/ziyadmsq/android/lowclub/ProfileEdit.java
@@ -0,0 +1,15 @@
+package com.ziyadmsq.android.lowclub;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ProfileEdit extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_edit);
+
+    }
+
+}
