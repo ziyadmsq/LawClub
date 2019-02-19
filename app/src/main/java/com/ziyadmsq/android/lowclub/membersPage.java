922f06469ba4d87ffaba43bee4fc541d607c249d
ziyadalqahtani <ziyadmsq@gmail.com>
2019-02-19 23:46:08 +0300

    trying out

---
+++ app/src/main/java/com/ziyadmsq/android/lowclub/membersPage.java
@@ -0,0 +1,14 @@
+package com.ziyadmsq.android.lowclub;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class membersPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_members_page);
+    }
+
+}
